1fb43b149c6d33780370e2e813f7d206e70897d3
Neysa Alarcon <neysa.alarcon@gmail.com>
2022-11-08 07:45:41 -0300

    feat: listening the connection of a client

--- sockets/server.js
+++ sockets/server.js
@@ -1,6 +1,14 @@
-const app = require('express')()
+const express = require('express');
+const app = express()
 const server = require('http').createServer(app);
 server.listen(3000);
 
-const socketio = require('socket.io')
-const io = socketio.listen(server)
+app.use(express.static('public'))
+
+const { Server } = require('socket.io'); 
+const io = new Server(server);
+io.listen(server);
+
+io.on('connect', function(socket){
+    console.log('nueva conexion id: ' + socket.id);
+})

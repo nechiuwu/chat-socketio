6da26c37ac4792e6d3569a4a37a92f0e530da0c3
Neysa Alarcon <neysa.alarcon@gmail.com>
2023-12-01 03:05:15 -0300

    feat: chat grupal working

--- public/funciones-socket.js
+++ public/funciones-socket.js
@@ -1,5 +1,21 @@
+const socket = io.connect();
+
+socket.on('nuevo_correo', function (datos) {
+    alert("nuevo correo conectado: " + datos.correo + ' con usuario: ' + datos.usuario)
+})
+
+socket.on('nuevo_mensaje', function (datos) {
+    $('#cont_mensajes').append('<p><strong>' + datos.usuario + ': </strong>' + datos.mensaje + '</p>')
+})
+
 function login() {
-    correo = $('#login_form  #correo').val(); 
+    correo = $('#login_form  #correo').val();
     usuario = $('#login_form  #usuario').val();
-    alert(correo+ ' - ' + usuario);
+    socket.emit('datos_usuario', { correo: correo, usuario: usuario });
+}
+
+function enviar_msj() {
+    mensaje = $('#mensaje').val();
+    usuario = $('#login_form  #usuario').val();
+    socket.emit('send_mensaje', { mensaje: mensaje, usuario: usuario });
 }
--- sockets/server.js
+++ sockets/server.js
@@ -5,14 +5,18 @@ server.listen(3000);
 
 app.use(express.static('public'))
 
-const { Server } = require('socket.io'); 
+const { Server } = require('socket.io');
 const io = new Server(server);
 io.listen(server);
 
-io.on('connect', function(socket){
+io.on('connect', function (socket) {
     console.log('nueva conexion id: ' + socket.id);
-    socket.on('datos_usuario', function(datos){
-        console.log('correo: ' + datos.correo);
-        io.emit('nuevo_correo', { correo: datos.correo});
+    socket.on('datos_usuario', function (datos) {
+        console.log('correo: ' + datos.correo + ' usuario: ' + datos.usuario);
+        io.emit('nuevo_correo', { correo: datos.correo, usuario: datos.usuario });
+    });
+    socket.on('send_mensaje', function (datos) {
+        console.log(datos.usuario + ' ha enviado un mensaje');
+        io.emit('nuevo_mensaje', { usuario: datos.usuario, mensaje: datos.mensaje });
     });
 })
